cf03d11269252837ff1811098e003efd2acce5da
Cameron Green <camerongreen@gmail.com>
2013-01-28 19:29:35 +1000

    Adding block for header image on sections

---
+++ modules/alq_section_home_blocks/theme/section-home-header.tpl.php
@@ -0,0 +1,10 @@
+<div id="section-home-header">
+<?php 
+  print theme_image_style(array(
+    "style_name" => "alq_section_header",
+    "path" => $image,
+    "height" => NULL,
+    "width" => NULL,
+  ));
+?>
+</div>
