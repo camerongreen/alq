0cfa67e0bd644b3a960ea6088c0d74f8b8edc2d4
Cameron Green <i@camerongreen.org>
2015-01-29 20:38:34 +1000

    Port should come from ssh config

--- scripts/backup_prod_files.sh
+++ scripts/backup_prod_files.sh
@@ -3,12 +3,11 @@
 ALQ_DOMAIN_NAME=alq.org.au
 ALQ_HOST_USER=
 ALQ_HOST=
-ALQ_HOST_PORT=
 ALQ_HOST_FILES_DIR=
 ALQ_FILES_DIR=
 WWW_GROUP=
 
 # pull the latest backups
-rsync -rve "ssh -p $ALQ_HOST_PORT" --delete --exclude='css' --exclude='js' ${ALQ_HOST_USER}@${ALQ_HOST}:${ALQ_HOST_FILES_DIR} $ALQ_FILES_DIR
+rsync -rve "ssh" --delete --exclude='css' --exclude='js' ${ALQ_HOST_USER}@${ALQ_HOST}:${ALQ_HOST_FILES_DIR} $ALQ_FILES_DIR
 chown -R $user:$WWW_GROUP $ALQ_FILES_DIR
 chmod -R g+w $ALQ_FILES_DIR
