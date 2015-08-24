1e1b309ae7c9a946af47ecc69e89ac094a9b8ca9
Cameron Green <i@camerongreen.org>
2015-08-24 21:34:25 +1000

    Tests passing, more to come

--- test/tests/test-donation-form.js
+++ test/tests/test-donation-form.js
@@ -5,14 +5,71 @@
  */
 var siteUrl = 'http://alq.test';
 
-module.exports = {
-  'Test membership options come up when monthly selected' : function (browser) {
-    browser
-      .url(siteUrl + '/demo/index.html')
-      .waitForElementVisible('body', 1000)
-      .click('#monthly')
-      .waitForElementVisible('#membership', 1000)
-      .assert.containsText('#membership', 'complimentary membership')
-      .end();
-  }
+this.testAmountOther = function (browser) {
+  browser
+    .url(siteUrl + '/demo/index.html')
+    .waitForElementVisible('#amountOther', 1000);
+  browser.expect.element('#amountOtherValue').to.not.be.enabled;
+  browser.expect.element('#amountOther input').to.not.be.selected;
+  browser.click('#amountOther');
+  browser.expect.element('#amountOther input').to.be.selected;
+  browser.expect.element('#amountOtherValue').to.be.enabled;
+
+  browser.setValue('#amountOtherValue', '50')
+    .getValue('#amountOtherValue', function (result) {
+      this.assert.equal(result.value, '50')
+    })
+    .end();
+};
+
+// with all this bootstrap magic going on, test trivial things
+this.testClickMonthly = function (browser) {
+  browser
+    .url(siteUrl + '/demo/index.html')
+    .waitForElementVisible('#monthly', 1000);
+
+  browser.expect.element('#oneoff input').to.be.selected;
+  browser.expect.element('#monthly input').to.not.be.selected;
+  browser.click('#monthly');
+  browser.expect.element('#monthly input').to.be.selected;
+  browser.expect.element('#oneoff input').to.not.be.selected;
+  browser.end();
+};
+
+this.testMembershipVisible = function (browser) {
+  browser
+    .url(siteUrl + '/demo/index.html')
+    .waitForElementVisible('#monthly', 1000)
+    .click('#monthly')
+    .waitForElementVisible('#membership', 1000)
+    .assert.containsText('#membership', 'complimentary membership')
+    .end();
+};
+
+this.testAmountOtherMembershipEligible = function (browser) {
+  browser
+    .url(siteUrl + '/demo/index.html')
+    .waitForElementVisible('#amountOther', 1000)
+    .click('#amountOther')
+    .waitForElementVisible('#amountOtherValue', 1000);
+
+  browser.expect.element('#amountOtherValue').to.be.enabled;
+
+  browser.setValue('#amountOtherValue', '50')
+    .click('#amountOther') // do this to move the focus
+    .waitForElementVisible('#membership', 1000)
+    .assert.containsText('#membership', 'complimentary membership')
+    .end();
+};
+
+this.testAmountOtherMembershipIneligible = function (browser) {
+  browser
+    .url(siteUrl + '/demo/index.html')
+    .waitForElementVisible('#amountOther', 1000)
+    .click('#amountOther')
+    .waitForElementVisible('#amountOtherValue', 1000)
+    .setValue('#amountOtherValue', '49')
+    .click('#amountOther') // do this to move the focus
+    .assert.hidden('#membership')
+    .end();
 };
