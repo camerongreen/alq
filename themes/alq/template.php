c556a8f1d3ad567f48d337e1ad1938c5a9c72fb6
Cameron Green <i@camerongreen.org>
2015-08-25 21:10:05 +1000

    Adding image for front page shares

--- themes/alq/template.php
+++ themes/alq/template.php
@@ -5,11 +5,31 @@
  * This file is empty by default because the base theme chain (Alpha & Omega) provides
  * all the basic functionality. However, in case you wish to customize the output that Drupal
  * generates through Alpha & Omega this file is a good place to do so.
- * 
+ *
  * Alpha comes with a neat solution for keeping this file as clean as possible while the code
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+/**
+ * Alter the page
+ */
+function alq_page_alter($page) {
+  global $base_url;
+
+  if (drupal_is_front_page()) {
+    $meta_og_image = array(
+      '#type' => 'html_tag',
+      '#tag' => 'meta',
+      '#attributes' => array(
+        'name' => 'og:image',
+        'content' => $base_url . '/' . drupal_get_path('theme', 'alq') . '/images/alq.jpg'
+      )
+    );
+
+    drupal_add_html_head($meta_og_image, 'meta_og_image');
+  }
+}
 
 /**
  * Process variables for search-result.tpl.php.
@@ -30,7 +50,7 @@ function alq_breadcrumb($variables) {
 
   if (!empty($breadcrumb)) {
     $breadcrumb = preg_replace("/Catalog/", "Shop", $breadcrumb); // This line will search and replace text.
-    return '<div class="breadcrumb">'. implode(' &raquo; ', $breadcrumb) . '</div>';
+    return '<div class="breadcrumb">' . implode(' &raquo; ', $breadcrumb) . '</div>';
   }
 
 }
