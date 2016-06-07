607572a58c3aa3306b1eecd01f62d677405d7924
Cameron Green <i@camerongreen.org>
2016-06-07 22:54:04 +1000

    Starting to come together

--- modules/alq_rodeos/theme/donation-section.tpl.php
+++ modules/alq_rodeos/theme/donation-section.tpl.php
@@ -1,18 +1,41 @@
 <a name="donate"></a>
 <div class="row">
-  <div class="col-sm-5">
+  <div class="col-sm-5" id="donation-form">
+    <h2>Donate</h2>
     <form>
-      <label for="donation_18"><input type="radio" name="donation_amount"
-                                      value="18" id="donation_18"/> $18</label>
-      <label for="donation_45"><input type="radio" name="donation_amount"
-                                      value="45" id="donation_45"/> $45</label>
-      <label for="donation_100"><input type="radio" name="donation_amount"
-                                       value="100" id="donation_100"/>
-        $100</label>
-      <label for="donation_other"><input type="radio" name="donation_amount"
-                                         value="other" id="donation_other"/>
-        Other</label>
-      <button type="button">Give now</button>
+      <div class="row">
+        <div class="col-sm-3">
+
+          <div class="form-group">
+            <label for="donation_18"><input type="radio" name="donation_amount"
+                                            value="18" id="donation_18"/>
+              $18</label>
+          </div>
+          <div class="form-group">
+            <label for="donation_45"><input type="radio" name="donation_amount"
+                                            value="45" id="donation_45"/>
+              $45</label>
+          </div>
+          <div class="form-group">
+            <label for="donation_100"><input type="radio" name="donation_amount"
+                                             value="100" id="donation_100"/>
+              $100</label>
+          </div>
+          <div class="form-group">
+            <label for="donation_other"><input type="radio"
+                                               name="donation_amount"
+                                               value="other"
+                                               id="donation_other"/>
+              Other</label>
+          </div>
+        </div>
+        <div class="col-sm-9">
+          <div>
+            Your generous donation will help found our important work
+          </div>
+          <button type="button" class="btn btn-default btn-lg">Give now</button>
+        </div>
+      </div>
     </form>
   </div>
   <div class="col-sm-7">
--- modules/alq_rodeos/theme/email-member-section.tpl.php
+++ modules/alq_rodeos/theme/email-member-section.tpl.php
@@ -1,11 +1,40 @@
 <a name="take-action"></a>
 <?= $take_action_block ?>
 <div class="row">
-  <div class="col-sm-6">
+  <div class="col-sm-7" id="email-form-section">
     <?= $email_intro_block ?>
     <?= $email_form ?>
   </div>
-  <div class="col-sm-6">
-    <?= $explainer_block ?>
+  <div class="col-sm-5" id="email-explainer">
+    <div class="well">
+      <?= $explainer_block ?>
+    </div>
+    <div id="take-action-images">
+      <div class="image-row">
+        <div>
+          <img
+            src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-1.jpg"
+            alt="Calf roping sequence 1" class='img-thumbnail'/>
+        </div>
+        <div>
+          <img
+            src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-2.jpg"
+            alt="Calf roping sequence 2" class="img-thumbnail"/>
+        </div>
+      </div>
+      <div class="image-row">
+        <div>
+
+          <img
+            src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-3.jpg"
+            alt="Calf roping sequence 3" class="img-thumbnail"/>
+        </div>
+        <div>
+          <img
+            src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-4.jpg"
+            alt="Calf roping sequence 4" class="img-thumbnail"/>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
