5071fec427d72a983300e44f59c345ab2afe9db0
Cameron Green <i@camerongreen.org>
2016-07-04 22:46:49 +1000

    Moving things out to bootstrap

--- modules/alq_rodeos/theme/email-member-section.tpl.php
+++ modules/alq_rodeos/theme/email-member-section.tpl.php
@@ -9,30 +9,26 @@
       <?= $explainer_block ?>
     </div>
   </div>
- <div class="row"> 
-    <div id="take-action-images">
-      <div class="image-row">
-        <div>
-          <img
-            src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-1.jpg"
-            alt="Calf roping sequence 1" class='img-thumbnail'/>
-        </div>
-        <div>
-          <img
-            src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-2.jpg"
-            alt="Calf roping sequence 2" class="img-thumbnail"/>
-        </div>
-        <div>
-          <img
-            src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-3.jpg"
-            alt="Calf roping sequence 3" class="img-thumbnail"/>
-        </div>
-        <div>
-          <img
-            src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-4.jpg"
-            alt="Calf roping sequence 4" class="img-thumbnail"/>
-        </div>
-      </div>
+  <div class="row" id="take-action-images">
+    <div class="col-sm-3 col-xs-6">
+      <img
+        src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-1.jpg"
+        alt="Calf roping sequence 1" class='img-thumbnail img-responsive'/>
+    </div>
+    <div class="col-sm-3 col-xs-6">
+      <img
+        src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-2.jpg"
+        alt="Calf roping sequence 2" class="img-thumbnail img-responsive"/>
+    </div>
+    <div class="col-sm-3 col-xs-6">
+      <img
+        src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-3.jpg"
+        alt="Calf roping sequence 3" class="img-thumbnail img-responsive"/>
+    </div>
+    <div class="col-sm-3 col-xs-6">
+      <img
+        src="/<?= drupal_get_path('theme', 'rodeos') ?>/images/small-calf-4.jpg"
+        alt="Calf roping sequence 4" class="img-thumbnail img-responsive"/>
     </div>
   </div>
 </div>
