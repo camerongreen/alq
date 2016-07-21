a87f01b54cd0236efe9ae31d8388ddfee50a101d
Cameron Green <i@camerongreen.org>
2016-07-21 11:02:17 +1000

    Design changes

--- modules/alq_rodeos/theme/footer.tpl.php
+++ modules/alq_rodeos/theme/footer.tpl.php
@@ -1,18 +1,19 @@
 <footer class="row">
-  <div class="col-sm-4">
+  <div class="col-sm-4 col-xs-12">
     <ul class="links">
       <li><a href="//alq.org.au">Animal Liberation Queensland website</a></li>
+      <li><a href="//alq.org.au/donate">Donate</a></li>
       <li><a href="//alq.org.au/contact">Contact ALQ</a></li>
       <li><a href="//alq.org.au/privacy">Privacy policy</a></li>
       </ul>
   </div>
-  <div class="col-sm-3 social-media">
+  <div class="col-sm-3 col-xs-12 social-media">
     <a href="https://www.facebook.com/animalliberationqld" target="_new" title="Facebook"><icon class="fa fa-facebook"></icon></a>
     <a href="https://twitter.com/animallibqld" target="_new" title="Twitter"><icon class="fa fa-twitter"></icon></a>
     <a href="//www.instagram.com/animallibqld/" target="_new" title="Instagram"><icon class="fa fa-instagram"></icon></a>
     <div>Connect with us</div>
   </div>
-  <div class="col-sm-5 copyright">
+  <div class="col-sm-5 col-xs-12 copyright">
     Copyright &copy; Animal Liberation Queensland 2016
   </div>
 </footer>
