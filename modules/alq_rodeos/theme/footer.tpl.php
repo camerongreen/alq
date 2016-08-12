0d0c8316f67747db4a64a0015be15baf313cd9c3
Cameron Green <i@camerongreen.org>
2016-08-12 14:14:49 +1000

    Adding rspca logo

--- modules/alq_rodeos/theme/footer.tpl.php
+++ modules/alq_rodeos/theme/footer.tpl.php
@@ -1,19 +1,31 @@
-<footer class="row">
-  <div class="col-sm-4 col-xs-12">
-    <ul class="links">
-      <li><a href="//alq.org.au">Animal Liberation Queensland website</a></li>
-      <li><a href="//alq.org.au/donate">Donate</a></li>
-      <li><a href="//alq.org.au/contact">Contact ALQ</a></li>
-      <li><a href="//alq.org.au/privacy">Privacy policy</a></li>
+<footer>
+  <div class="row">
+    <div class="col-sm-4 col-xs-12">
+      <ul class="links">
+        <li><a href="//alq.org.au">Animal Liberation Queensland website</a></li>
+        <li><a href="//alq.org.au/donate">Donate</a></li>
+        <li><a href="//alq.org.au/contact">Contact ALQ</a></li>
+        <li><a href="//alq.org.au/privacy">Privacy policy</a></li>
       </ul>
+    </div>
+    <div class="col-sm-3 col-xs-12 social-media">
+      <a href="https://www.facebook.com/animalliberationqld" target="_new"
+         title="Facebook"><icon class="fa fa-facebook"></icon></a>
+      <a href="https://twitter.com/animallibqld" target="_new"
+         title="Twitter"><icon class="fa fa-twitter"></icon></a>
+      <a href="//www.instagram.com/animallibqld/" target="_new"
+         title="Instagram"><icon class="fa fa-instagram"></icon></a>
+      <div>Connect with us</div>
+    </div>
+    <div class="col-sm-5 col-xs-12 images">
+      <!--img
+        src="<?= drupal_get_path('theme', 'alq') ?>/images/alq_logo_white.png"
+        alt="ALQ Logo"/-->
+      <img src="<?= drupal_get_path('theme', 'rodeos') ?>/images/rspca.png"
+           alt="RSPCA Logo"/>
+    </div>
   </div>
-  <div class="col-sm-3 col-xs-12 social-media">
-    <a href="https://www.facebook.com/animalliberationqld" target="_new" title="Facebook"><icon class="fa fa-facebook"></icon></a>
-    <a href="https://twitter.com/animallibqld" target="_new" title="Twitter"><icon class="fa fa-twitter"></icon></a>
-    <a href="//www.instagram.com/animallibqld/" target="_new" title="Instagram"><icon class="fa fa-instagram"></icon></a>
-    <div>Connect with us</div>
-  </div>
-  <div class="col-sm-5 col-xs-12 copyright">
+  <div class="copyright">
     Copyright &copy; Animal Liberation Queensland 2016
   </div>
 </footer>
