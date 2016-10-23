a25a74739ffca99e0fe69e227b7ae6b6b783ed30
Cameron Green <i@camerongreen.org>
2016-10-23 23:04:38 +1000

    Adding ads menu

--- themes/rodeos/templates/region--branding.tpl.php
+++ themes/rodeos/templates/region--branding.tpl.php
@@ -2,14 +2,16 @@
   <div<?php print $content_attributes; ?>>
     <?php if ($linked_logo_img || $site_name || $site_slogan): ?>
       <div class="branding-data clearfix row">
-        <ul role="menu" class="menu-cnt col-sm-6 col-xs-12">
+        <ul role="menu" class="menu-cnt col-sm-9 col-xs-12">
           <li class="nobullet"><a href="/#block-alq-rodeos-alq-rodeos-email-member" class="anke">Take Action</a></li>
           <li><a href="/info">Rodeo Info</a></li>
           <li><a href="/#block-alq-rodeos-alq-rodeos-donation" class="anke">Donate</a></li>
+          <li class="highlight desktop"><a href="/censored-ads">Censored Ads</a></li>
+          <li class="highlight mobile"><a href="/censored-ads">Ads</a></li>
           <li><a href="/" title="Home"><icon class="fa fa-home"></icon></a></li>
         </ul>
         <?php if ($linked_logo_img): ?>
-          <div class="logo-img col-sm-6 hidden-xs">
+          <div class="logo-img col-sm-3 hidden-xs">
             <?php print $linked_logo_img; ?>
           </div>
         <?php endif; ?>
