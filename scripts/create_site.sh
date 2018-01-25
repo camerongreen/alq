f488cdaf62da3715f11ed969450557c22454ff2b
Cameron Green <i@camerongreen.org>
2018-01-25 11:31:12 +1000

    Allowing things to be passed in via cli

--- scripts/create_site.sh
+++ scripts/create_site.sh
@@ -29,7 +29,7 @@ DEFAULT_DB_HOST=alq_db
 
 # hosting company work around has custom drush path
 if [ -f ~/.drush_alias ]; then
-        . ~/.drush_alias
+  . ~/.drush_alias
 fi
 
 #
@@ -59,44 +59,56 @@ if [ -z ${ADMIN} ]
 fi
 
 # set up the database
-read -p "Database user [${DEFAULT_DB_USER}]:" DB_USER
 if [ -z ${DB_USER} ]
-  then
-    DB_USER=${DEFAULT_DB_USER}
+  read -p "Database user [${DEFAULT_DB_USER}]:" DB_USER
+  if [ -z ${DB_USER} ]
+    then
+      DB_USER=${DEFAULT_DB_USER}
+  `fi
 fi
 
-read -s -p "Database (and Drupal admin user) passwd:" DB_PASSWD
-# need a newline in the output here as -s swallows it
-echo ""
 if [ -z ${DB_PASSWD} ]
-  then
-    echo "DB Password is required";
-    exit 1
+  read -s -p "Database (and Drupal admin user) passwd:" DB_PASSWD
+  # need a newline in the output here as -s swallows it
+  echo ""
+  if [ -z ${DB_PASSWD} ]
+    then
+      echo "DB Password is required";
+      exit 1
+  fi
 fi
-read -p "Database host [${DEFAULT_DB_HOST}]:" DB_HOST
+
 if [ -z ${DB_HOST} ]
-  then
-    DB_HOST=${DEFAULT_DB_HOST}
+  read -p "Database host [${DEFAULT_DB_HOST}]:" DB_HOST
+  if [ -z ${DB_HOST} ]
+    then
+      DB_HOST=${DEFAULT_DB_HOST}
+  fi
 fi
-read -p "Database name [${DEFAULT_DB_NAME}]:" DB_NAME
+
 if [ -z ${DB_NAME} ]
-  then
-    DB_NAME=${DEFAULT_DB_NAME}
+  read -p "Database name [${DEFAULT_DB_NAME}]:" DB_NAME
+  if [ -z ${DB_NAME} ]
+    then
+      DB_NAME=${DEFAULT_DB_NAME}
+  fi
 fi
 
 echo "SELECT 1;" | mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASSWD} ${DB_NAME} > /dev/null
 command_status "Unable to connect to database\nPlease ensure you have created ${DB_NAME} and granted access to ${DB_USER}@${DB_HOST}" "Connected to db";
 
-read -p "Site email [${ADMIN_EMAIL}]:" SITE_EMAIL
 if [ -z ${SITE_EMAIL} ]
-  then
-    if [ -z ${ADMIN_EMAIL} ]
-      then
-        echo "Site needs an email, see the comments at the top of"
-        echo "this script for how to put a default one on command line"
-        exit 1
-    else
-      SITE_EMAIL=${ADMIN_EMAIL}
+  read -p "Site email [${ADMIN_EMAIL}]:" SITE_EMAIL
+  if [ -z ${SITE_EMAIL} ]
+    then
+      if [ -z ${ADMIN_EMAIL} ]
+        then
+          echo "Site needs an email, see the comments at the top of"
+          echo "this script for how to put a default one on command line"
+          exit 1
+      else
+        SITE_EMAIL=${ADMIN_EMAIL}
+    fi
   fi
 fi
 
