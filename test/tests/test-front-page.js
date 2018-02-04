df3a2d201ef746758c4c8890f128ce1018c9103f
Cameron Green <i@camerongreen.org>
2018-02-04 17:33:23 +1000

    Adding check that will pass

--- test/tests/test-front-page.js
+++ test/tests/test-front-page.js
@@ -4,12 +4,19 @@
 var siteUrl = 'http://localhost';
 
 module.exports = {
-  'Check footer year current' : function (browser) {
+  'Check title': function (browser) {
+    browser
+        .url(siteUrl)
+        .waitForElementVisible('body', 1000)
+        .assert.containsText('h1', 'ANIMAL LIBERATION QUEENSLAND')
+        .end();
+  },
+  'Check footer year current': function (browser) {
     var date = new Date();
     browser
-      .url(siteUrl)
-      .waitForElementVisible('body', 1000)
-      .assert.containsText('#section-footer', 'Animal Liberation Queensland ' + date.getFullYear())
-      .end();
+        .url(siteUrl)
+        .waitForElementVisible('body', 1000)
+        .assert.containsText('#section-footer', 'Animal Liberation Queensland ' + date.getFullYear())
+        .end();
   }
 };
