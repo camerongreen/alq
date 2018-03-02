9adc26df8f12b074561fdf5704aedae2c9ad572f
Cameron Green <i@camerongreen.org>
2018-03-02 20:48:56 +1000

    Using alias

--- scripts/backup_prod_files.sh
+++ scripts/backup_prod_files.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # This backs up the Drupal site uploaded files using rsync from production to my machine
-ALQ_DOMAIN_NAME=alq.org.au
-ALQ_HOST_USER=
-ALQ_HOST=
+ALQ_SSH_ALIAS=alq
 ALQ_HOST_FILES_DIR=
 ALQ_FILES_DIR=
 WWW_GROUP=
@@ -12,7 +10,7 @@ sudo chown -R $user:$WWW_GROUP $ALQ_FILES_DIR
 sudo chmod -R g+w $ALQ_FILES_DIR
 
 # pull the latest backups
-rsync -rve "ssh" --delete --exclude='css' --exclude='js' ${ALQ_HOST_USER}@${ALQ_HOST}:${ALQ_HOST_FILES_DIR} $ALQ_FILES_DIR
+rsync -rve "ssh" --delete --exclude='css' --exclude='js' ${ALQ_SSH_ALIAS}:${ALQ_HOST_FILES_DIR} $ALQ_FILES_DIR
 
 # chown the files that are imported, prob unecessary here :)
 sudo chown -R $user:$WWW_GROUP $ALQ_FILES_DIR
