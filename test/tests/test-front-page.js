8ddca95746fd0cfa6817d9ebee5ae4ed269bb8b5
Cameron Green <i@camerongreen.org>
2018-03-03 21:28:29 +1000

    Why....why

--- test/tests/test-front-page.js
+++ test/tests/test-front-page.js
@@ -3,17 +3,18 @@
  */
 module.exports = {
   'Check title': function (browser) {
-    console.log('lu:' + browser.launch_url);
+    var siteUrl = browser.launch_url;
     browser
-        .url(browser.launch_url)
+        .url(browser.siteUrl)
         .waitForElementVisible('body', 1000)
-        .assert.containsText('h1', 'ANIMAL LIBERATION QUEENSLAND')
+        .assert.containsText('h1', 'OUR LATEST NEWS')
         .end();
   },
   'Check footer year current': function (browser) {
+    var siteUrl = browser.launch_url;
     var date = new Date();
     browser
-        .url(browser.launch_url)
+        .url(siteUrl)
         .waitForElementVisible('body', 1000)
         .assert.containsText('#section-footer', 'Animal Liberation Queensland ' + date.getFullYear())
         .end();
