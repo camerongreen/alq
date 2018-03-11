2ea9edc5b887f1a78b5064a7eea988993821ab63
Cameron Green <i@camerongreen.org>
2018-03-11 21:44:30 +1000

    Riddles in the dark

--- test/tests/test-email-campaign.js
+++ test/tests/test-email-campaign.js
@@ -24,12 +24,12 @@ this.testSearchBySuburb = function (browser) {
   var siteUrl = browser.launch_url;
   browser
       .url(siteUrl + '/' + pageUrl)
-      .waitForElementVisible('#edit-suburb', 1000);
+      .waitForElementVisible('ul.ui-autocomplete', 1000);
 
   browser.expect.element('h1').text.to.equal('BAN GREYHOUND RACING IN QUEENSLAND');
-  //browser.expect.element('ul.ui-autocomplete').to.not.be.visible;
+  browser.expect.element('ul.ui-autocomplete').to.not.be.visible;
   browser.setValue('#edit-suburb', 'Highgate');
-  //browser.waitForElementVisible('ul.ui-autocomplete li', 10000);
+  browser.waitForElementVisible('ul.ui-autocomplete li', 1000);
   browser.expect.element('ul.ui-autocomplete li a').text.to.contain('HIGHGATE HILL');
   browser.click('ul.ui-autocomplete li a');
   browser.expect.element('#edit-emailee-name').value.to.equal('Hon Jacklyn Trad');
@@ -42,12 +42,12 @@ this.testSearchByPostcode = function (browser) {
   var siteUrl = browser.launch_url;
   browser
       .url(siteUrl + '/' + pageUrl)
-      .waitForElementVisible('#edit-suburb', 1000);
+      .waitForElementVisible('ul.ui-autocomplete', 1000);
 
   browser.expect.element('h1').text.to.equal('BAN GREYHOUND RACING IN QUEENSLAND');
-  //browser.expect.element('ul.ui-autocomplete').to.not.be.visible;
+  browser.expect.element('ul.ui-autocomplete').to.not.be.visible;
   browser.setValue('#edit-suburb', '4101');
-  //browser.waitForElementVisible('ul.ui-autocomplete li', 10000);
+  browser.waitForElementVisible('ul.ui-autocomplete li', 1000);
   browser.expect.element('ul.ui-autocomplete li a').text.to.contain('HIGHGATE HILL');
   browser.click('ul.ui-autocomplete li a');
   browser.expect.element('#edit-emailee-name').value.to.equal('Hon Jacklyn Trad');
