178ec4c73e082a40ab82bf97717cdd3272fc9842
Cameron Green <i@camerongreen.org>
2018-04-12 22:09:04 +1000

    Adding assert to make sure on right page

--- test/tests/test-shop.js
+++ test/tests/test-shop.js
@@ -15,10 +15,11 @@ this.testAddToCart = function (browser) {
   var price = null;
   browser.getText('span.uc-price', function (result) {
     price = result.value.substr(1);
-    browser.clearValue('#edit-qty')
-    browser.setValue('#edit-qty', '2')
-    browser.click('#edit-submit-230')
-    browser.click('#edit-checkout--2')
+    browser.clearValue('#edit-qty');
+    browser.setValue('#edit-qty', '2');
+    browser.click('#edit-submit-230');
+    browser.assert.urlContains('/cart');
+    browser.click('#edit-checkout--2');
     browser.expect.element('td.subtotal span.uc-price').text.to.equal('$' + (2 * price).toFixed(2));
     browser.end();
   });
