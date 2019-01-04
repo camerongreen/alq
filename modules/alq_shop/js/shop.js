d6e7070ad824ba77b4c8a0d7cb49ca7c690b1c6c
Cameron Green <i@camerongreen.org>
2019-01-05 09:09:52 +1000

    Theming product page

---
+++ modules/alq_shop/js/shop.js
@@ -0,0 +1,19 @@
+(function ($) {
+  $(document).ready(function () {
+    $('div.field-type-image div.field-items').after('<ul id="slide-nav">').cycle({
+      fx:     'fade',
+      speed:  'slow',
+      pager:  '#slide-nav',
+
+      // callback fn that creates a thumbnail to use as pager anchor
+      pagerAnchorBuilder: function(idx, slide) {
+        var img = $(slide).find('img')[0];
+        return '<li><a href="#"><img class="img-thumbnail" src="' + img.src + '" /></a></li>';
+      }
+    });
+
+    $('.img-thumbnail').click(function() {
+      $('div.field-type-image div.field-items').cycle('pause');
+    });
+  });
+})(jQuery);
