361443c74cc2c3fe8b136fce5dfb22440d474078
Cameron Green <i@camerongreen.org>
2019-01-06 09:30:31 +1000

    Slideshow funkiness

--- modules/alq_shop/js/shop.js
+++ modules/alq_shop/js/shop.js
@@ -1,20 +1,20 @@
 (function ($) {
-  $(document).ready(function () {
+  $(window).on('load', function () {
     $('div.field-type-image div.field-items').after('<ul id="slide-nav">').cycle({
-      fx:     'fade',
-      speed:  'slow',
-      pager:  '#slide-nav',
+      fx: 'fade',
+      speed: 'slow',
+      pager: '#slide-nav',
       slideResize: true,
       containerResize: true,
 
       // callback fn that creates a thumbnail to use as pager anchor
-      pagerAnchorBuilder: function(idx, slide) {
+      pagerAnchorBuilder: function (idx, slide) {
         var img = $(slide).find('img')[0];
         return '<li><a href="#"><img class="img-thumbnail" src="' + img.src + '" /></a></li>';
       }
     });
 
-    $('.img-thumbnail').click(function() {
+    $('.img-thumbnail').click(function () {
       $('div.field-type-image div.field-items').cycle('pause');
     });
   });
