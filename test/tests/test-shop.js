5d485a633d48954bd74484e3cd6293721b981948
Cameron Green <i@camerongreen.org>
2019-02-27 22:36:40 +1000

    Fixing test

--- test/tests/test-shop.js
+++ test/tests/test-shop.js
@@ -9,9 +9,9 @@ this.testAddToCart = function (browser) {
       .url(siteUrl + '/' + pageUrl);
   browser.useXpath();
   browser.click('//a[text()="Apparel"]');
-  browser.click('//a[text()="Be the Change T-Shirt"]');
+  browser.click('//a[text()="ALQ Hoodie"]');
   browser.useCss();
-  browser.click('select[id="edit-attributes-2"] option[value="5"]')
+  browser.click('select[id="edit-attributes-38"] option[value="129"]')
   var price = null;
   browser.getText('span.uc-price', function (result) {
     price = result.value.substr(1);
