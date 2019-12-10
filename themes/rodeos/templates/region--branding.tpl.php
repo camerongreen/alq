c51cfe67cc0496b1d84434a6b2a3b3a1858963bc
Cameron Green <i@camerongreen.org>
2019-12-10 23:01:42 +1000

    Updating donate link

--- themes/rodeos/templates/region--branding.tpl.php
+++ themes/rodeos/templates/region--branding.tpl.php
@@ -12,7 +12,7 @@
         <ul role="menu" class="menu-cnt col-sm-9 col-xs-12">
           <li class="nobullet"><a href="/#block-alq-rodeos-alq-rodeos-email-member" class="anke">Take Action</a></li>
           <li><a href="/info">Rodeo Info</a></li>
-          <li><a href="/#block-alq-rodeos-alq-rodeos-donation" class="anke">Donate</a></li>
+          <li><a href="http://alq.org.au/donate" target="_new">Donate</a></li>
           <li class="highlight"><a href="/news" aria-haspopup="true">News</a>
               <ul aria-label="submenu">
                   <li><a href="/cruelty-is-not-great">Email Kate Jones</a></li>
