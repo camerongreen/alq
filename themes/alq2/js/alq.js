81d989d97e80b2d046e75d431dac72ea7cb40b88
Cameron Green <i@camerongreen.org>
2020-03-26 10:26:15 +1000

    Adding copy of alq theme

---
+++ themes/alq2/js/alq.js
@@ -0,0 +1,38 @@
+(function($){
+  "use strict";
+
+  // Fix for https://stackoverflow.com/questions/14923301/uncaught-typeerror-cannot-read-property-msie-of-undefined-jquery-tools
+  jQuery.browser = {};
+  (function () {
+    jQuery.browser.msie = false;
+    jQuery.browser.version = 0;
+    if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
+      jQuery.browser.msie = true;
+      jQuery.browser.version = RegExp.$1;
+    }
+  })();
+
+  $(document).ready(function () {
+    $("body.front .views-slideshow-controls-text span").hide();
+
+    $('#search-block-form input[type=text]').attr('placeholder', 'Search');
+    $('#webform-client-form-114 input[type=email]').attr('placeholder', 'Your email');
+
+    // Mobile
+    if (!$("#block-facebook-boxes-fb-like:visible").length) {
+    }
+
+
+    $(".view-news .views-field-field-image img").addClass("img-polaroid");
+
+    // desktop
+    if ($("#block-facebook-boxes-fb-like:visible").length) {
+      $("body.front .view-slideshow").hover(function () {
+        $(".views-slideshow-controls-text span:not(:eq(1))").fadeIn();
+      },
+      function () {
+        $(".views-slideshow-controls-text span:not(:eq(1))").fadeOut();
+      });
+    }
+  });
+})(jQuery);
