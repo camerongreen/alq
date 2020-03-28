0f40b444788a94706a7b902deb11685f1e8cb1b7
Cameron Green <i@camerongreen.org>
2020-03-28 23:18:51 +1000

    Much messing around with scss

---
+++ themes/alq/templates/region--header-first.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Override of Alpha's theme implementation to display a zone.
+ *
+ * Here we just get rid of all the guff which creates a grid so we can
+ * have full width for the slideshow.
+ */
+?>
+<?php if ($wrapper): ?>
+<div class="region-header-first">
+  <?php print $content; ?>
+</div>
