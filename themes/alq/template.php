f90e203c11a8157cb1526f128e08fcaf01e616ca
Cameron Green <i@camerongreen.org>
2020-04-05 18:45:45 +1000

    Compiling libaries in with source

--- themes/alq/template.php
+++ themes/alq/template.php
@@ -22,32 +22,25 @@ function alq_preprocess_html(&$vars) {
   drupal_add_library('system', 'ui.autocomplete');
   drupal_add_library('system', 'jquery.cookie');
 
-  // Make sure boostrap appears after existing CSS.
-  $weight = 200;
-
-  drupal_add_js('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', ['type' => 'external']);
-
   // Add fonts.
   drupal_add_css('//fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;1,400&display=swap', [
     'type' => 'external',
   ]);
-  drupal_add_css('//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', [
-    'type' => 'external',
-  ]);
 
-  // Add bootstrap cdn.
-  drupal_add_css('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', [
-    'type' => 'external',
-    'weight' => ++$weight,
-  ]);
-  drupal_add_css('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css', [
-    'type' => 'external',
-    'weight' => ++$weight,
-  ]);
+  // Add bootstrap and bootstrap flat.
+  drupal_add_js(path_to_theme() . '/js/bootstrap-3.3.7.min.js');
+
+  // Make sure boostrap appears after existing CSS.
+  $weight = 200;
+
   drupal_add_css(path_to_theme() . '/css/bootstrap-flat.min.css', [
+    'group' => CSS_THEME,
+    'preprocess' => FALSE,
     'weight' => ++$weight,
   ]);
   drupal_add_css(path_to_theme() . '/css/bootstrap-flat-extras.min.css', [
+    'group' => CSS_THEME,
+    'preprocess' => FALSE,
     'weight' => ++$weight,
   ]);
 }
