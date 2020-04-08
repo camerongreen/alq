8a401bcc2021bdb511dab08ff11bbe47a45cfd34
Cameron Green <i@camerongreen.org>
2020-04-08 19:30:06 +1000

    Slightly wider slideshow

---
+++ themes/alq/templates/region--header-first.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Override of Alpha's theme implementation to display a zone.
+ *
+ * Here we just get rid of all the guff which creates a grid so we can
+ * have full width for the slideshow.
+ */
+?>
+<div class="region-header-first">
+  <?php print $content; ?>
+</div>
