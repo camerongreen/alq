cdb1b10053d8b63c267fea7e573900924cb87dea
Cameron Green <i@camerongreen.org>
2020-05-03 20:15:56 +1000

    Promo block alpha

--- modules/alq_blocks/theme/promo-block.tpl.php
+++ modules/alq_blocks/theme/promo-block.tpl.php
@@ -8,12 +8,18 @@
 $file = file_load($image);
 $image_url = file_create_url($file->uri);
 
+$text_classes = [
+  'text',
+  $alignment,
+  $background,
+];
+
 ?>
 <div class="jumbotron" style="background-image: url(<?php print $image_url ?>)">
     <div class="container">
-        <div class="text" style="text-align: <?php print $alignment ?>">
+        <div class="<?php print implode(' ', $text_classes) ?>">
           <?php print $text['value'] ?>
-            <p><a class="btn btn-warning btn-lg"
+            <p><a class="btn btn-<?php print $button_type; ?> btn-lg"
                   href="<?php print $button_url ?>"
                   role="button"><?php print $button_text ?></a></p>
         </div>
