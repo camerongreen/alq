f2ed7d32544403ca59c0e4fae4316f1d13dfbc2f
Cameron Green <i@camerongreen.org>
2020-05-15 16:59:39 +1000

    Trying to get mobile menu working

--- themes/alq/js/alq.js
+++ themes/alq/js/alq.js
@@ -2,6 +2,37 @@
   "use strict";
 
   $(document).ready(function () {
+    // Observe mega menu and hide other icons in header.
+    const meanBar = $('#block-nice-menus-1');
+
+    // Things to hide.
+    const hide = $('.social-media');
+
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true };
+
+    // Callback function to execute when mutations are observed
+    const callback = function (mutationsList, observer) {
+      // Use traditional 'for loops' for IE 11
+      for (let mutation of mutationsList) {
+        if (mutation.type === 'attributes') {
+          if ($('.meanclose').length) {
+            $(hide).hide();
+          } else {
+            setTimeout(() => {
+              if ($('.meanclose').length === 0) {
+                $(hide).show()
+              }
+            }, 500);
+          }
+        }
+      }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(meanBar.get(0), config);
+    //observer.disconnect();
+
 
   });
 })(jQuery);
