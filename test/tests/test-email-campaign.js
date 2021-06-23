7332f22a87a7cb6a5c2e2830cc56c1e84f771c90
Cameron Green <i@camerongreen.org>
2021-06-23 19:32:06 +1000

    Updating test

--- test/tests/test-email-campaign.js
+++ test/tests/test-email-campaign.js
@@ -15,7 +15,8 @@ this.testDefaultMinister = function (browser) {
 
   browser.click('#suburb-default', function() {
     this.expect.element('#edit-emailee-electorate').to.not.be.visible;
-    this.expect.element('#edit-emailee-name').to.have.value.that.equals('Hon Annastacia Palaszczuk');
+    //this.expect.element('#edit-emailee-name').to.have.value.that.equals('Hon Annastacia Palaszczuk');
+    this.expect.element('#edit-emailee-name').to.have.value.that.equals('Minister for Agriculture');
   });
   browser.end();
 };
@@ -27,13 +28,13 @@ this.testSearchBySuburb = function (browser) {
       .url(siteUrl + pageUrl)
       .waitForElementPresent('ul.ui-autocomplete', 1000);
 
-  browser.expect.element('h1').text.to.equal('Ban greyhound racing in Queensland');
+  browser.expect.element('h1').text.to.be.present.
   browser.expect.element('ul.ui-autocomplete').to.be.present;
   browser.setValue('#edit-suburb', 'Highgate')
   browser.waitForElementVisible('ul.ui-autocomplete li a', 30000);
   browser.expect.element('ul.ui-autocomplete li a').text.to.contain('HIGHGATE HILL');
   browser.click('ul.ui-autocomplete li a');
-  browser.expect.element('#edit-emailee-name').value.to.equal('Hon Jacklyn Trad');
+  browser.expect.element('#edit-emailee-name').value.to.not.be.empty;
   browser.expect.element('#edit-emailee-electorate').value.to.equal('SOUTH BRISBANE');
   browser.end();
 };
@@ -45,14 +46,14 @@ this.testSearchByPostcode = function (browser) {
       .url(siteUrl + pageUrl)
       .waitForElementPresent('ul.ui-autocomplete', 1000);
 
-  browser.expect.element('h1').text.to.equal('Ban greyhound racing in Queensland');
+  browser.expect.element('h1').text.to.be.present;
   browser.expect.element('ul.ui-autocomplete').to.be.present;
   browser.expect.element('ul.ui-autocomplete').to.not.be.visible;
   browser.setValue('#edit-suburb', '4101');
   browser.waitForElementVisible('ul.ui-autocomplete li', 30000);
   browser.expect.element('ul.ui-autocomplete li a').text.to.contain('HIGHGATE HILL');
   browser.click('ul.ui-autocomplete li a');
-  browser.expect.element('#edit-emailee-name').value.to.equal('Hon Jacklyn Trad');
+  browser.expect.element('#edit-emailee-name').value.to.not.be.empty;
   browser.expect.element('#edit-emailee-electorate').value.to.equal('SOUTH BRISBANE');
   browser.end();
 };
