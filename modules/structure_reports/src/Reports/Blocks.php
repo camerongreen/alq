25ace5c78dfdb966a15648fddf20c936f945b19a
Cameron Green <i@camerongreen.org>
2021-12-16 21:56:19 +1000

    Updating deps

--- modules/structure_reports/src/Reports/Blocks.php
+++ modules/structure_reports/src/Reports/Blocks.php
@@ -10,21 +10,30 @@ class Blocks {
   /**
    * Run audit of blocks on site.
    *
+   * @param int $status
+   *   Filters by the block status.
+   *
    * @return array
    *   Array of information about blocks on the site.
    */
-  public static function report(): array {
+  public static function report(int $status = NULL): array {
+
     $query = db_select('block', 'b')
       ->fields('b')
       ->orderBy('module')
       ->orderBy('theme')
-      ->orderBy('delta')
-      ->execute()
+      ->orderBy('delta');
+
+    if (!is_null($status)) {
+      $query->condition('status', $status);
+    }
+
+    $result = $query->execute()
       ->fetchAll();
 
     $rows = [];
 
-    foreach ($query as $block) {
+    foreach ($result as $block) {
       $rows[] = [
         dt('Title') => $block->title,
         dt('Delta') => $block->delta,
