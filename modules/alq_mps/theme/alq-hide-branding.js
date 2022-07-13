d0de58f92c1d0e1c664cfab55490ada417e34caa
Cameron Green <i@camerongreen.org>
2022-07-13 18:02:06 +1000

    Adding css for faster removal, with js as backup

---
+++ modules/alq_mps/theme/alq-hide-branding.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Functions to support the alp mps application.
+ */
+
+(($) => {
+  $(document).ready(() => {
+    // Hide things.
+    $('h1.page-title, h2.block-title, .section-header, .section-footer, #breadcrumb, .block-addtoany').hide();
+    // Remove some margins.
+    $('.zone-content').css({
+      margin: '1rem'
+    })
+  });
+})(jQuery);
